edaee0ebab556fa8f822055a8cebb580a1a05637
br41nd34d <danil.pashkov.45@gmail.com>
2025-10-29 22:14:56 +0500

    some fixes

--- run2.cs
+++ run2.cs
@@ -7,41 +7,81 @@ class Program
     static List<string> Solve(List<(string, string)> edges)
     {
         var result = new List<string>();
-        var edgesDict = new Dictionary<string, HashSet<string>>();
+        var adj = new Dictionary<string, HashSet<string>>();
         foreach (var edgePair in edges)
         {
-            if (!edgesDict.ContainsKey(edgePair.Item1))
-                edgesDict[edgePair.Item1] = [];
-            edgesDict[edgePair.Item1].Add(edgePair.Item2);
-            if (!edgesDict.ContainsKey(edgePair.Item2))
-                edgesDict[edgePair.Item2] = [];
-            edgesDict[edgePair.Item2].Add(edgePair.Item1);
+            if (!adj.ContainsKey(edgePair.Item1)) adj[edgePair.Item1] = new HashSet<string>();
+            adj[edgePair.Item1].Add(edgePair.Item2);
+            if (!adj.ContainsKey(edgePair.Item2)) adj[edgePair.Item2] = new HashSet<string>();
+            adj[edgePair.Item2].Add(edgePair.Item1);
         }
 
-        var gates = edgesDict
-            .Where(x => char.IsUpper(x.Key[0]))
-            .Select(x => x.Key)
-            .ToHashSet();
+        var gates = adj.Keys.Where(x => char.IsUpper(x[0])).ToHashSet();
         var position = "a";
         while (true)
         {
-            var targetPath = FindPathToClosestGate(edgesDict, position, gates);
-            if (targetPath.Length == 0)
+            if (FindPathToClosestGate(adj, position, gates).Length == 0)
                 break;
-            
-            var edgeToCLose = string.Join("-", targetPath[^1].Item2, targetPath[^1].Item1);
-            edgesDict[targetPath[^1].Item1].Remove(targetPath[^1].Item2);
-            edgesDict[targetPath[^1].Item2].Remove(targetPath[^1].Item1);
-            var movePath = FindPathToClosestGate(edgesDict, position, gates);
-            if (movePath.Length > 0)
-                position = movePath[0].Item2;
-            result.Add(edgeToCLose);
+            var possibleMoves = new List<string>();
+            foreach (var gate in gates.OrderBy(g => g))
+                if (adj.TryGetValue(gate, out var value))
+                    foreach (var neighbor in value.OrderBy(n => n))
+                        possibleMoves.Add($"{gate}-{neighbor}");
+
+            string bestMove = null;
+            foreach (var move in possibleMoves)
+                if (IsWinningMove(adj, gates, position, move))
+                {
+                    bestMove = move;
+                    break;
+                }
+
+            var parts = bestMove.Split('-');
+            var node1 = parts[0];
+            var node2 = parts[1];
+            adj[node1].Remove(node2);
+            adj[node2].Remove(node1);
+            result.Add(bestMove);
+            var virusMovePath = FindPathToClosestGate(adj, position, gates);
+            if (virusMovePath.Length > 0)
+                position = virusMovePath[0].Item2;
         }
 
         return result;
     }
 
-    private static (string, string)[] FindPathToClosestGate(Dictionary<string, HashSet<string>> edgesDict, string position,
+    private static bool IsWinningMove(
+        Dictionary<string, HashSet<string>> currentAdj,
+        ISet<string> gates,
+        string currentVirusPos,
+        string move)
+    {
+        var adjCopy = new Dictionary<string, HashSet<string>>();
+        foreach (var item in currentAdj)
+            adjCopy[item.Key] = [..item.Value];
+        var parts = move.Split('-');
+        adjCopy[parts[0]].Remove(parts[1]);
+        adjCopy[parts[1]].Remove(parts[0]);
+        var virusMovePath = FindPathToClosestGate(adjCopy, currentVirusPos, gates);
+        if (virusMovePath.Length > 0)
+            currentVirusPos = virusMovePath[0].Item2;
+
+        while (true)
+        {
+            var path = FindPathToClosestGate(adjCopy, currentVirusPos, gates);
+            if (path.Length == 0) return true;
+            var gateToCut = path.Last().Item2;
+            var nodeToCut = path.Last().Item1;
+            adjCopy[gateToCut].Remove(nodeToCut);
+            adjCopy[nodeToCut].Remove(gateToCut);
+            var nextVirusPath = FindPathToClosestGate(adjCopy, currentVirusPos, gates);
+            if (nextVirusPath.Length > 0)
+                currentVirusPos = nextVirusPath[0].Item2;
+        }
+    }
+
+    private static (string, string)[] FindPathToClosestGate(Dictionary<string, HashSet<string>> edgesDict,
+        string position,
         ISet<string> gates)
     {
         var pathsToGates = new List<(string, string)[]>();
@@ -51,6 +91,7 @@ class Program
             if (path.Length > 0)
                 pathsToGates.Add(path);
         }
+
         if (pathsToGates.Count == 0)
             return Array.Empty<(string, string)>();
         pathsToGates
@@ -83,14 +124,17 @@ class Program
                     pathEdges.Add((visited[node], node));
                     node = visited[node];
                 }
+
                 pathEdges.Reverse();
                 return pathEdges.ToArray();
             }
+
             if (edgesDict.TryGetValue(current, out var value))
                 foreach (var neighbor in value.OrderBy(n => n))
                     if (visited.TryAdd(neighbor, current))
                         queue.Enqueue(neighbor);
         }
+
         return [];
     }
 
